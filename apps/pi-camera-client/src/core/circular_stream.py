ad345590d0bd299309bade63458152db90c1f95d
Duy Lam <duylamphuong@gmail.com>
2021-06-20 15:23:22 +0700

    test

--- apps/pi-camera-client/src/core/circular_stream.py
+++ apps/pi-camera-client/src/core/circular_stream.py
@@ -6,33 +6,35 @@ import picamera
 
 class CircularStream:
     def __init__(self, buffer_size):
-        self.circular_io = picamera.CircularIO(buffer_size)
-        self.num_bytes_written = 0
+        self._circular_io = picamera.CircularIO(buffer_size)
+        self._circular_io.seek(0)
+        self._num_bytes_written = 0
 
     #
-    # Implement the interface that aiortc.MediaPlayer requires
+    # Implement the file-like reading interface that aiortc.MediaPlayer requires
     # See: https://github.com/PyAV-Org/PyAV/blob/9ac05d9ac902d71ecb2fe80f04dcae454008378c/av/container/core.pyx#L171
     #
     @property
     def writable(self):
         return False
 
-    def read(n=-1):
+    def read(self, n=-1):
         # Simulate the behavior from
         # https://docs.python.org/3/library/io.html#io.RawIOBase.read
-        if self.num_bytes_written == 0:
+        if self._num_bytes_written == 0:
           return None
-        self.circular_io.seek(-1 * self.num_bytes_written, 1)
-        self.circular_io.read1(n)
-        self.num_bytes_written = 0
+        self._circular_io.seek(-1 * self._num_bytes_written, 1)
+        self._num_bytes_written = 0
+        return self._circular_io.read1(n)
 
     #
     # END
     #
 
+    # We don't want to expose write() so that aiortc.MediaPlayer thinks this is read-only stream
     def write_(self, byte_s):
-        self.num_bytes_written = self.num_bytes_written + self.cicular_io.write(byte_s)
+        self._num_bytes_written = self._num_bytes_written + self._circular_io.write(byte_s)
 
     def close():
-       self.circular_stream.close() 
+       self._circular_io.close()
 
--- apps/pi-camera-client/test/core/test_circular_stream.py
+++ apps/pi-camera-client/test/core/test_circular_stream.py
@@ -3,7 +3,25 @@ import unittest
 from core import CircularStream
 
 class TestCircularStream(unittest.TestCase):
-    def test_ok(self):
-        stream = CircularStream()
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_write_(self):
+        stream = CircularStream(buffer_size=5)
+        write_content = b'\x01\x02'
+        stream.write_(write_content)
+        read_bytes = stream.read()
+        self.assertIsNotNone(read_bytes, 'read() should return data')
+        self.assertEqual(len(read_bytes), 2)
+        self.assertEqual(read_bytes, write_content)
+
+        stream = CircularStream(buffer_size=3)
+        stream.write_(b'\x01\x02\x03\x04\x05')
+        read_bytes = stream.read()
+        self.assertEqual(len(read_bytes), 2)
+        self.assertEqual(read_bytes, b'\x04\x05')
+
+    def test_read_when_writing_once(self):
+        True
+
+
+    def test_read_when_writing_cicle(self):
+        True
 
