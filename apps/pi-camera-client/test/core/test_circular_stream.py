5c3a9da30a4c986378508623928e83829837478e
Duy Lam <duylamphuong@gmail.com>
2021-06-20 16:50:34 +0700

    90done

--- apps/pi-camera-client/test/core/test_circular_stream.py
+++ apps/pi-camera-client/test/core/test_circular_stream.py
@@ -12,16 +12,24 @@ class TestCircularStream(unittest.TestCase):
         self.assertEqual(len(read_bytes), 2)
         self.assertEqual(read_bytes, write_content)
 
-        stream = CircularStream(buffer_size=3)
-        stream.write_(b'\x01\x02\x03\x04\x05')
+        write_content = b'\x03\x04'
+        stream.write_(write_content)
+        read_bytes = stream.read()
+        self.assertEqual(len(read_bytes), 2)
+        self.assertEqual(read_bytes, write_content)
+
+        write_content = b'\x05\x06\x07'
+        stream.write_(write_content)
         read_bytes = stream.read()
         self.assertEqual(len(read_bytes), 3)
-        self.assertEqual(read_bytes, b'\x03\x04\x05')
+        self.assertEqual(read_bytes, write_content)
 
-    def test_read_when_writing_once(self):
-        True
-
-
-    def test_read_when_writing_cicle(self):
-        True
+    def test_read_behaviors(self):
+        stream = CircularStream(buffer_size=3)
+        write_content = b'\x01\x02'
+        stream.write_(write_content)
+        read_bytes = stream.read()
+        self.assertIsNotNone(read_bytes, 'read() should return data')
+        read_bytes = stream.read()
+        self.assertIsNone(read_bytes)
 
