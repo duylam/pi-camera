04dcdac7f70b77342542eed7b5b30acd54b61802
Duy Lam <duylam@kobiton.com>
2021-08-01 16:34:01 +0700

    a

---
+++ apps/webrtc-signaling-server/scripts/dockerize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker_image_name=${PI_MEETING_DOCKER_IMAGE_NAME-webrtc-signaling} 
+docker_image_tag=${PI_MEETING_DOCKER_IMAGE_TAG-latest}
+
+npm run build
+
+cd docker
+rm -rf app || true
+mkdir app
+cp -r ../build/ ./app/
+docker build --label "commit.id=`git rev-parse --short HEAD`" --label "created.time=`date -u '+%Y-%m-%dT%H:%M:%S'`" --tag $docker_image_name:$docker_image_tag .
+
