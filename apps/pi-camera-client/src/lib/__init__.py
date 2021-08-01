0e0788625cf0abe164a937e72ead4c12afc7e221
Duy Lam <duylamphuong@gmail.com>
2021-08-01 22:28:41 +0700

    done

--- apps/pi-camera-client/src/lib/__init__.py
+++ apps/pi-camera-client/src/lib/__init__.py
@@ -1,6 +1,6 @@
 from .rtc import RtcConnection
 
-# from .camera import Camera
+from .camera import Camera
 # Use in development to feed VideoFrame to the RTCConnection. This class
 # expose same interface  with above Camera class
-from .file_camera import Camera
+#from .file_camera import Camera
