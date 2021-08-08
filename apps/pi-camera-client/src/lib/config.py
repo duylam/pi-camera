fe7067cdf697a79c8384bcdc78d69a51b671fc77
Duy Lam <duylamphuong@gmail.com>
2021-08-08 21:58:27 +0700

    done

--- apps/pi-camera-client/src/lib/config.py
+++ apps/pi-camera-client/src/lib/config.py
@@ -11,7 +11,6 @@ GRPC_HOSTNAME = os.getenv('PI_MEETING_GRPC_HOSTNAME', 'localhost')
 GRPC_PORT = int(os.getenv('PI_MEETING_GRPC_PORT', 4000))
 MAIN_TASK_INTERVAL_DURATION = int(
     os.getenv('PI_MEETING_MAIN_TASK_INTERVAL_DURATION_MS', 100))
-FRAMERATE = int(os.getenv('PI_MEETING_FRAME_PER_SECOND', 20))
 CAMERA_BUFFER_SIZE = int(
     os.getenv('PI_MEETING_CAMERA_BUFFER_SIZE_IN_KB', 5*1024))*const.KB
 ROOT_LOGGING_NAMESPACE = os.getenv(
@@ -20,4 +19,11 @@ ROOT_LOGGING_NAMESPACE = os.getenv(
 # See https://docs.python.org/3/library/logging.html?highlight=logging#logging-levels
 LOG_LEVEL_NUM = int(os.getenv('PI_MEETING_LOGGING_LEVEL_NUM', logging.DEBUG))
 
-VIDEO_RESOLUTION = (1024, 768)  # (width, height)
+FRAMERATE = int(os.getenv('PI_MEETING_FRAME_PER_SECOND', 24))
+VIDEO_RESOLUTION = (640, 480)  # (width, height)
+
+# The quantization option for video
+# For the 'h264' format, use values between 10 and 40 where 10 is extremely high quality,
+# and 40 is extremely low (20-25 is usually a reasonable range for H.264 encoding).
+# See https://picamera.readthedocs.io/en/release-1.3/api.html#picamera.PiCamera.start_recording
+VIDEO_QUANTIZATION_OPTION = int(os.getenv('PI_MEETING_VIDEO_QUANTIZATION_OPTION', 15))
