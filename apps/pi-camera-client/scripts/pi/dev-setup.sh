e2601ffaf827abe141499a21148f60fd021516a0
Duy Lam <duylamphuong@gmail.com>
2021-08-29 18:13:42 +0700

    Fix: remove unused code

--- apps/pi-camera-client/scripts/pi/dev-setup.sh
+++ apps/pi-camera-client/scripts/pi/dev-setup.sh
@@ -25,12 +25,3 @@ if ! command -v ffmpeg -v quiet &> /dev/null; then
   sudo make install
 fi
 
-# Python3 required
-if ! command -v python3 &> /dev/null; then
-  echo "* Installing python3"
-  sudo apt install python3
-fi
-
-# Required while installing python libs
-sudo apt-get install -y libffi-dev
-
